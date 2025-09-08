a0ad5f5d8babadba40598e0ad284ee0304b98ded
Kira <hanzomein228@gmail.com>
2025-09-08 18:06:00 +0300

    пофиксиил получение фильтров

--- src/utils/get-exceptions.ts
+++ src/utils/get-exceptions.ts
@@ -1,6 +1,12 @@
 import { TPreset } from "./get-presets";
 
 export const getFilters = (preset: TPreset, field: string, rule: `exceptions` | `default_filters`) => {
+  if (!preset || !preset[rule]) {
+    return []; // или другое значение по умолчанию
+  }
+  if (!Array.isArray(preset[rule])) {
+    return []; // или другое значение по умолчанию
+  }
   const exception = preset[rule].find(f => f.field === field)
   return exception ? exception.values : []
 }
